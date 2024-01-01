525bec2e218cd2b7dc1877730fce448470530b25
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-12-31 21:06:39 -0600

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('app:start-conversation')->dailyAt('20:00');
     }
 
     /**

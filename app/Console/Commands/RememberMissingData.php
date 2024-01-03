17f174df30604c0508b130062f5bafe6a1653dce
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-01-03 11:06:09 -0600

    Update RememberMissingData.php

--- app/Console/Commands/RememberMissingData.php
+++ app/Console/Commands/RememberMissingData.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\User;
 use App\Notifications\MissingAnswers;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class RememberMissingData extends Command
 {
@@ -27,6 +28,8 @@ class RememberMissingData extends Command
      */
     public function handle()
     {
+        Log::info('Remembering missing data');
+
         $users = User::all();
 
         $users
